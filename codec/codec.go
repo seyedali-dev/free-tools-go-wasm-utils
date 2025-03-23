b7c28919d3457d63b0e374d4bedb3d28c00da003
SeyedAli <seyed.ali.devl@gmail.com>
2025-03-23 16:43:32 +0330

    codec decode

--- codec/codec.go
+++ codec/codec.go
@@ -8,5 +8,9 @@ import (
 // ImageCodec is a common interface for encoding/decoding images.
 type ImageCodec interface {
 	Encode(writer io.Writer, img image.Image, options map[string]interface{}) error // Encode encodes an image to the given writer.
-	Decode(reader io.Reader, options map[string]interface{}) (image.Image, error)   // Decode decodes an image from the given reader.
+}
+
+// Decode decodes an image with default image.Decode decoder from the given reader. It returns the decoded image, format and error if occurred.
+func Decode(reader io.Reader, options map[string]interface{}) (image.Image, string, error) {
+	return image.Decode(reader)
 }
